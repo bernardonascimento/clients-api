7b2fd3ce8bd000f195c52454001367b389c3a574
bernardonascimento <bernardonasciimento@gmail.com>
2025-04-11 02:14:49 -0300

    Mudando segredo

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 
-const SECRET = "meu_segredo_super_secreto";
+const SECRET = "SECRET_TOKEN";
 
 const USER = {
   email: "admin@gmail.com",
